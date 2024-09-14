51bbd12a827afc2ca79d5be24b0d4fb2dc58a828
hekt <hektorg@gmail.com>
2024-09-14 22:56:18 +0900

    .

--- go.mod
+++ go.mod
@@ -17,8 +17,6 @@ require (
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
-	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
@@ -30,10 +28,10 @@ require (
 	golang.org/x/oauth2 v0.22.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	google.golang.org/api v0.196.0 // indirect
+	google.golang.org/genproto v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.66.0 // indirect
--- main.go
+++ main.go
@@ -7,11 +7,48 @@ import (
 	"log"
 	"os"
 
-	speech "cloud.google.com/go/speech/apiv1"
-	"cloud.google.com/go/speech/apiv1/speechpb"
+	speech "cloud.google.com/go/speech/apiv2"
+	"cloud.google.com/go/speech/apiv2/speechpb"
 )
 
-const outputFilePath = "output.txt"
+const (
+	outputFilePath = "output.txt"
+	recognizerName = "myrecognizer"
+)
+
+func createRecognizer() {
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	project := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	parent := fmt.Sprintf("projects/%s/locations/global", project)
+
+	_, err = client.CreateRecognizer(ctx, &speechpb.CreateRecognizerRequest{
+		Parent:       parent,
+		RecognizerId: recognizerName,
+		Recognizer: &speechpb.Recognizer{
+			DisplayName: "default-recognizer",
+			DefaultRecognitionConfig: &speechpb.RecognitionConfig{
+				Model:         "long",
+				LanguageCodes: []string{"ja-jp"},
+				DecodingConfig: &speechpb.RecognitionConfig_ExplicitDecodingConfig{
+					ExplicitDecodingConfig: &speechpb.ExplicitDecodingConfig{
+						Encoding:          speechpb.ExplicitDecodingConfig_LINEAR16,
+						SampleRateHertz:   16000,
+						AudioChannelCount: 1,
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+		fmt.Printf("Failed to create recognizer: %v", err)
+	}
+}
 
 func writeFinalResult(s string) {
 	file, err := os.OpenFile(outputFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -26,6 +63,28 @@ func writeFinalResult(s string) {
 	}
 }
 
+func createStream(ctx context.Context, client *speech.Client) (speechpb.Speech_StreamingRecognizeClient, error) {
+	stream, err := client.StreamingRecognize(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
+		Recognizer: fmt.Sprintf("projects/%s/locations/global/recognizers/%s", os.Getenv("GOOGLE_CLOUD_PROJECT"), recognizerName),
+		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
+			StreamingConfig: &speechpb.StreamingRecognitionConfig{
+				StreamingFeatures: &speechpb.StreamingRecognitionFeatures{
+					InterimResults: true,
+				},
+			},
+		},
+	}); err != nil {
+		return nil, err
+	}
+
+	return stream, nil
+}
+
 func main() {
 	ctx := context.Background()
 
@@ -33,27 +92,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	stream, err := client.StreamingRecognize(ctx)
+	stream, err := createStream(ctx, client)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Send the initial configuration message.
-	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
-			StreamingConfig: &speechpb.StreamingRecognitionConfig{
-				Config: &speechpb.RecognitionConfig{
-					Encoding:        speechpb.RecognitionConfig_LINEAR16,
-					SampleRateHertz: 16000,
-					LanguageCode:    "ja-JP",
-					Model:           "latest_long",
-				},
-				InterimResults: true,
-			},
-		},
-	}); err != nil {
-		log.Fatal(err)
-	}
 
+	// todo おそらく stream が閉じた直後に呼ばれるとエラーが発生して死ぬので、エラーハンドリングが必要
 	go func() {
 		// Pipe stdin to the API.
 		buf := make([]byte, 1024)
@@ -61,8 +105,8 @@ func main() {
 			n, err := os.Stdin.Read(buf)
 			if n > 0 {
 				if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-					StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
-						AudioContent: buf[:n],
+					StreamingRequest: &speechpb.StreamingRecognizeRequest_Audio{
+						Audio: buf[:n],
 					},
 				}); err != nil {
 					log.Printf("Could not send audio: %v", err)
@@ -90,23 +134,31 @@ func main() {
 		if err != nil {
 			log.Fatalf("Cannot stream results: %v", err)
 		}
-		if err := resp.Error; err != nil {
-			// Workaround while the API doesn't give a more informative error.
-			if err.Code == 3 || err.Code == 11 {
-				log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
-			}
-			log.Fatalf("Could not recognize: %v", err)
-		}
+		// if err := resp.Error; err != nil {
+		// 	// Workaround while the API doesn't give a more informative error.
+		// 	if err.Code == 3 || err.Code == 11 {
+		// 		log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
+		// 	}
+		// 	log.Fatalf("Could not recognize: %v", err)
+		// }
 
 		// clear screen
 		fmt.Print("\033[H\033[2J")
 		t := ""
 		for _, result := range resp.Results {
 			s := result.Alternatives[0].Transcript
+			t += s
 			if result.IsFinal {
 				writeFinalResult(s)
+				if err := stream.CloseSend(); err != nil {
+					log.Fatalf("Could not close stream: %v", err)
+				}
+				stream, err = createStream(ctx, client)
+				if err != nil {
+					log.Fatal(err)
+				}
+				break
 			}
-			t += s
 		}
 
 		fmt.Println(t)
