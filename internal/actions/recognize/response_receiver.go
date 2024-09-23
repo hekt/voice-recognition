128ee3d5ec093ea45f5ad94504945f1ca8a85c79
hekt <hektorg@gmail.com>
2024-09-23 14:43:52 +0900

    improve canceled context handling

--- internal/actions/recognize/response_receiver.go
+++ internal/actions/recognize/response_receiver.go
@@ -48,18 +48,21 @@ func (r *ResponseReceiver) Start(ctx context.Context) error {
 		default:
 			resp, err := stream.Recv()
 			if err == io.EOF {
-				slog.Debug("ResponseReceiver: EOF received")
-
 				// when the stream is closed by the sender, the receiver will receive EOF after the final response.
 				// at that time, switch to new stream.
-				newStream, ok := <-r.receiveStreamCh
-				if !ok {
-					return fmt.Errorf("failed to get new receive stream from channel")
-				}
+				slog.Debug("ResponseReceiver: EOF received")
 
-				stream = newStream
-				slog.Debug("ResponseReceiver: stream switched")
-				continue
+				select {
+				case newStream, ok := <-r.receiveStreamCh:
+					if !ok {
+						return fmt.Errorf("failed to get new receive stream from channel")
+					}
+					stream = newStream
+					slog.Debug("ResponseReceiver: stream switched")
+					continue
+				case <-ctx.Done():
+					return nil
+				}
 			}
 			if status.Code(err) == codes.Canceled {
 				// when the command is terminated by SIGINT, context canceled occurs in the stream.
